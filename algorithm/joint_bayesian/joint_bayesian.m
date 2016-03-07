d4d8a03dba94d9ee16b569ae3391d45a5fa1c779
zjj <503917315@qq.com>
2016-03-07 19:52:22 +0800

    resolve a careless mistake in the implementation of Joint Bayesian
    Former-commit-id: 63088c0ad6fce67a9aa113b31481688d2e8aa9ec

--- algorithm/joint_bayesian/joint_bayesian.m
+++ algorithm/joint_bayesian/joint_bayesian.m
@@ -80,7 +80,7 @@ for i = 1:500
     for k = 1:num_class
         num_instance = size(instances_in_each_class{k}, 1);
         u(:, k) = sum(SuFG{num_instance} * instances_in_each_class{k}', 2);
-        w(:, j + num_instance - 1) = bsxfun(@plus, instances_in_each_class{k}', u(:, k));
+        w(:, j + num_instance - 1) = bsxfun(@plus, instances_in_each_class{k}', sum(SwG(num_instance) * instances_in_each_class{k}', 2));
         j = j + num_instance;
     end
     Su = cov(u');
